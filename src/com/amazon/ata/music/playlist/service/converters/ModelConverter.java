03c8fd3b9513f1c92122aa7dc0c615b06069fd1c
Ryan Krauss <rlkrauss02@gmail.com>
2024-11-10 22:28:39 -0500

    srpint 13 done i think

--- src/com/amazon/ata/music/playlist/service/converters/ModelConverter.java
+++ src/com/amazon/ata/music/playlist/service/converters/ModelConverter.java
@@ -1,5 +1,7 @@
 package com.amazon.ata.music.playlist.service.converters;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import com.amazon.ata.music.playlist.service.models.PlaylistModel;
 import com.amazon.ata.music.playlist.service.dynamodb.models.Playlist;
 
@@ -10,8 +12,18 @@ public class ModelConverter {
      * @return the converted playlist
      */
     public PlaylistModel toPlaylistModel(Playlist playlist) {
+        List<String> tags;
+        if (!playlist.getTags().isEmpty()) {
+            tags = new ArrayList<>(playlist.getTags());
+        } else {
+            tags = null;
+        }
         return PlaylistModel.builder()
-            .withId(playlist.getId())
-            .build();
+                .withId(playlist.getId())
+                .withName(playlist.getName())
+                .withCustomerId(playlist.getCustomerId())
+                .withSongCount(playlist.getSongCount())
+                .withTags(tags)
+                .build();
     }
 }
